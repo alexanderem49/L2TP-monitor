57fefa255f621a061e141e7a8cbbf31f6b5e6c3a
alexanderem49 <alexanderem49@gmail.com>
2024-01-31 01:00:21 +0400

    add encoding param

--- l2tp-monitor.py
+++ l2tp-monitor.py
@@ -24,7 +24,7 @@ def getInterfaceTotals(interface):
   result   = process.communicate()
   
   r_ipconfig = re.compile(r"RX bytes:(\d+) .+  TX bytes:(\d+)")
-  for line in result[0].split(bytes('\n')):
+  for line in result[0].split(bytes('\n', 'UTF-8')):
     m_ipconfig = r_ipconfig.search(line)
     if m_ipconfig:
       return (int(m_ipconfig.group(2)), int(m_ipconfig.group(1)))
